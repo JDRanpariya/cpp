47d01d54eec638fd4b3775e8d933de1bbba0aa99
jdranpariya <jdpatel.code@gmail.com>
2020-11-12 19:58:22 +0530

    hello world added

---
+++ input_output.cpp
@@ -0,0 +1,10 @@
+#include<iostream> // pre-processor Directive used to include files
+using namespace std; // std::cout to cout 
+
+
+int main(){ // execution of code begins from main function
+
+    cout<<"Hello Input\n"; // cout is used to display output
+
+    return 0; // exit status of function
+}
---
+++ test1.cpp
@@ -0,0 +1,10 @@
+/*! \brief Brief function description here
+ *
+ *  Detailed description
+ *
+ * \param Parameter Parameter description
+ * \return Return parameter description
+ */
+void test1::memberFunction(Type Parameter) {
+    
+}
