ed3a9b81a9ad983edbc1f4df8e627c4aa7a508ee
jdranpariya <jdpatel.code@gmail.com>
2020-11-12 20:40:40 +0530

    input_output2 added

---
+++ input_output2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int amount1;
+    cin >> amount1;
+
+    int amount2;
+    cin >> amount2;
+
+    int sum2;
+    sum2 = amount1 + amount2;
+
+    cout << sum2 << endl; // if you don't use endl it will show 2 + 3 as 5% and won't end the line
+    return 0;
+}
