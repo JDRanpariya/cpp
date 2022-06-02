569dc83bcf5fe8478e79f6e5a51f2d9c8b23fd64
JDRanpariya <51157407+JDRanpariya@users.noreply.github.com>
2022-06-02 12:03:11 +0530

    Create largest_of_three.cpp

---
+++ Intro to Cpp/largest_of_three.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Write your code here
+    int a,b,c;
+    cin >> a >> b >> c;
+    if(a>b && a>c){
+        cout << a;
+    }
+    else if(b>c && b>a){
+        cout << b;
+    }
+    else{
+        cout << c;
+    }
+
+    return 0;
+}
