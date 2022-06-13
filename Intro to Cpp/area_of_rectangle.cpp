5d5dee92305c3f5309f49f56629818ac1e628fde
JDRanpariya <51157407+JDRanpariya@users.noreply.github.com>
2022-06-13 17:56:46 +0530

    Create area_of_rectangle.cpp

---
+++ Intro to Cpp/area_of_rectangle.cpp
@@ -0,0 +1,13 @@
+class Rectangle {
+    // Write your code here.
+    
+    public:
+    int length,breadth;
+    Rectangle(){
+        length=0;
+        breadth=0;
+    }
+    long getArea(){
+        return length*breadth;
+    }
+};
